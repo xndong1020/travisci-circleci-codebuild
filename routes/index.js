981f7c7f52186c91d5c999142f34780f7da26401
isdance <isdance2004@hotmail.com>
2019-11-09 15:25:49 +1100

    add buildspec.yml

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get("/", function(req, res, next) {
   res.render("index", {
     title: "Express",
-    change: "Add manual approval to CodePipeline"
+    change: "Add manual approval to CodePipeline",
+    port: process.env.PORT
   });
 });
 
